00e1f9e4d7c0b41a03bd682994d5a4f3ce524ea8
aidahoo69 <aidahooerpyke@gmail.com>
2024-11-16 00:24:51 +0100

    Add files via upload

---
+++ personajes.js
@@ -0,0 +1,66 @@
+const personajesData = {
+    "seele": {
+        name: "Personaje 1",
+        image: "images/seele.png",
+        introduction: "Descripción breve del personaje 1...",
+        abilities: {
+            basicAttack: "Descripción del ataque básico.",
+            skill: "Descripción de la habilidad.",
+            ultimate: "Descripción de la habilidad definitiva.",
+            talent: "Descripción del talento.",
+            technique: "Descripción de la técnica."
+        },
+        majorTracks: [
+            { name: "Rastro Mayor 1", description: "Descripción del primer rastro mayor." },
+            { name: "Rastro Mayor 2", description: "Descripción del segundo rastro mayor." },
+            { name: "Rastro Mayor 3", description: "Descripción del tercer rastro mayor." }
+        ],
+        minorTracks: [
+            { name: "Rastro Menor 1", stat: "HP", value: "10" },
+            { name: "Rastro Menor 2", stat: "ATK", value: "5" },
+            { name: "Rastro Menor 3", stat: "Speed", value: "3" }
+        ],
+        eidolons: [
+            { name: "Eidolón 1", description: "Descripción del primer eidolón." },
+            { name: "Eidolón 2", description: "Descripción del segundo eidolón." },
+            { name: "Eidolón 3", description: "Descripción del tercer eidolón." },
+            { name: "Eidolón 4", description: "Descripción del cuarto eidolón." },
+            { name: "Eidolón 5", description: "Descripción del quinto eidolón." },
+            { name: "Eidolón 6", description: "Descripción del sexto eidolón." }
+        ],
+        stats: { HP: 80, ATK: 100, DEF: 60, Speed: 50 }
+    },
+
+    "bronya": {
+        name: "Personaje2",
+        image: "images/bronya.png",
+        introduction: "Descripción breve del personaje 2...",
+        abilities: {
+            basicAttack: "Descripción del ataque básico.",
+            skill: "Descripción de la habilidad.",
+            ultimate: "Descripción de la habilidad definitiva.",
+            talent: "Descripción del talento.",
+            technique: "Descripción de la técnica."
+        },
+        majorTracks: [
+            { name: "Rastro Mayor 1", description: "Descripción del primer rastro mayor." },
+            { name: "Rastro Mayor 2", description: "Descripción del segundo rastro mayor." },
+            { name: "Rastro Mayor 3", description: "Descripción del tercer rastro mayor." }
+        ],
+        minorTracks: [
+            { name: "Rastro Menor 1", stat: "HP", value: "10" },
+            { name: "Rastro Menor 2", stat: "ATK", value: "5" },
+            { name: "Rastro Menor 3", stat: "Speed", value: "3" }
+        ],
+        eidolons: [
+            { name: "Eidolón 1", description: "Descripción del primer eidolón." },
+            { name: "Eidolón 2", description: "Descripción del segundo eidolón." },
+            { name: "Eidolón 3", description: "Descripción del tercer eidolón." },
+            { name: "Eidolón 4", description: "Descripción del cuarto eidolón." },
+            { name: "Eidolón 5", description: "Descripción del quinto eidolón." },
+            { name: "Eidolón 6", description: "Descripción del sexto eidolón." }
+        ],
+        stats: { HP: 80, ATK: 100, DEF: 60, Speed: 50 }
+    },
+    
+};
